b5df00449b574859d77463e5be97b7cc754cdab5
Lee dong ho <dong963939@gmail.com>
2023-02-12 22:58:47 +0900

    :fire: 추가로 수정.
    추가로 수정.

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,16 @@ import Header from "@components/Header/Header";
 export default function App(): JSX.Element {
   return (
     <div className="App">
+      <Header></Header>
       <div>
-        <a href="https://vitejs.dev" target="blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a href="https://ko.reactjs.org/docs/hooks-intro.html" target="blank">
+          <img src="https://images.emojiterra.com/twitter/v13.1/512px/1fa9d.png" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>React Hooks Example</h1>
-      <Header></Header>
       <Routes>
         <Route path="/" element={<></>} />
         <Route path="/useRef1" element={<RefMain />} />
